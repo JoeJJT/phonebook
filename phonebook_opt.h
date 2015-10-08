f72e05996470fd2a30dc27004b54c14956587a81
JoeJJT <ssa35214@gmail.com>
2015-10-08 18:54:18 +0800

    Optimal Version 1 	separate lastname & other detail

--- phonebook_opt.h
+++ phonebook_opt.h
@@ -1,29 +1,8 @@
 #ifndef _PHONEBOOK_H
 #define _PHONEBOOK_H
-
+#include "phonebook_detail.h"
 #define MAX_LAST_NAME_SIZE 16
-
-/* original version */
-/*typedef struct __PHONE_BOOK_LAST_NAME {
-	char lastName[MAX_LAST_NAME_SIZE];
-	struct __PHONE_BOOK_ENTRY *detail;
-	struct __PHONE_BOOK_LAST_NAME *pNext;
-}LastNameEntry;
-*/
-typedef struct __PHONE_BOOK_ENTRY {
-	char lastName[MAX_LAST_NAME_SIZE];
-    char firstName[16];
-    char email[16];
-    char phone[10];
-    char cell[10];
-    char addr1[16];
-    char addr2[16];
-    char city[16];
-    char state[2];
-    char zip[5];
-    struct __PHONE_BOOK_ENTRY *pNext;
-}entry;
-
+/* optimal version1 */
 typedef struct __PHONE_BOOK_LAST_NAME {
 	char lastName[MAX_LAST_NAME_SIZE];
 	entry *detail;
