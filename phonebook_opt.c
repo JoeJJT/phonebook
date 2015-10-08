c48f0e29b47d72961a8ff145e439dbf5df2049f3
JoeJJT <ssa35214@gmail.com>
2015-10-08 16:44:59 +0800

    Phonebook Optimal 	modified: phonebook_opt.h & phonebook_opt.c     Use new structure to save "lastname" and link to others.     decrease cache miss

--- phonebook_opt.c
+++ phonebook_opt.c
@@ -1,15 +1,28 @@
 #include <stdlib.h>
-
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 #include "phonebook_opt.h"
 
 /* FILL YOUR OWN IMPLEMENTATION HERE! */
-entry *findName(char lastname[], entry *pHead)
+LastNameEntry *findName(char lastname[], LastNameEntry *pHead)
 {
     /* TODO: implement */
+	while(pHead != NULL) {
+		if(strcasecmp(lastname,pHead->lastName) == 0){
+			printf("%s is found!\n",lastname);
+			return pHead;
+		}
+		pHead = pHead->pNext;
+	}
     return NULL;
 }
 
-entry *append(char lastName[], entry *e)
+LastNameEntry *append(char lastName[], LastNameEntry *e)
 {
-    return NULL;
+	e->pNext = (LastNameEntry *)malloc (sizeof(LastNameEntry));
+	e = e->pNext;
+	strcpy(e->lastName,lastName);
+	e->pNext = NULL;
+    return e;
 }
--- phonebook_opt.h
+++ phonebook_opt.h
@@ -4,8 +4,14 @@
 #define MAX_LAST_NAME_SIZE 16
 
 /* original version */
+/*typedef struct __PHONE_BOOK_LAST_NAME {
+	char lastName[MAX_LAST_NAME_SIZE];
+	struct __PHONE_BOOK_ENTRY *detail;
+	struct __PHONE_BOOK_LAST_NAME *pNext;
+}LastNameEntry;
+*/
 typedef struct __PHONE_BOOK_ENTRY {
-    char lastName[MAX_LAST_NAME_SIZE];
+	char lastName[MAX_LAST_NAME_SIZE];
     char firstName[16];
     char email[16];
     char phone[10];
@@ -16,9 +22,15 @@ typedef struct __PHONE_BOOK_ENTRY {
     char state[2];
     char zip[5];
     struct __PHONE_BOOK_ENTRY *pNext;
-} entry;
+}entry;
 
-entry *findName(char lastname[], entry *pHead);
-entry *append(char lastName[], entry *e);
+typedef struct __PHONE_BOOK_LAST_NAME {
+	char lastName[MAX_LAST_NAME_SIZE];
+	entry *detail;
+	struct __PHONE_BOOK_LAST_NAME *pNext;
+}LastNameEntry;
+
+LastNameEntry *findName(char lastname[], LastNameEntry *pHead);
+LastNameEntry *append(char lastName[], LastNameEntry *e);
 
 #endif
