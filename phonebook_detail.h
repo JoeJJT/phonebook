573485b0ec69ae47cf452f67dc78d459f85d4ca1
JoeJJT <ssa35214@gmail.com>
2015-10-11 16:59:13 +0800

    Phonebook detail 	use Astyle to format coding style

--- phonebook_detail.h
+++ phonebook_detail.h
@@ -1,15 +1,15 @@
 
 #define MAX_LAST_NAME_SIZE 16
 typedef struct __PHONE_BOOK_ENTRY {
-     char firstName[MAX_LAST_NAME_SIZE];
-     char email[16];
-     char phone[10];
-     char cell[10];
-     char addr1[16];
-     char addr2[16];
-     char city[16];
-     char state[2];
-	 char zip[5];
-     struct __PHONE_BOOK_ENTRY *pNext;
-}entry;
-				 
+    char firstName[MAX_LAST_NAME_SIZE];
+    char email[16];
+    char phone[10];
+    char cell[10];
+    char addr1[16];
+    char addr2[16];
+    char city[16];
+    char state[2];
+    char zip[5];
+    struct __PHONE_BOOK_ENTRY *pNext;
+} entry;
+
